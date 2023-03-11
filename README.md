f066a31a6b4146060e189714b56834ffa0b5f826
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-03-11 00:36:44 +0000

    Base Functionality Complete

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Game-Of-Life-Visualisation
-A Typescript Simulation of Conway's Game of Life Cellular Automata Model
+# Conway's Game of Life - Cellular Automaton Visualisation
+
+Conway's Game of Life is one of the most notable forms of cellular automaton, modelled somewhat on population, where a cell changes value dependant on those around it and rules such as:
+
+1. _Any live cell with fewer than two live neighbours dies, as if by underpopulation._
+
+2. _Any live cell with two or three live neighbours lives on to the next generation._
+
+3. _Any live cell with more than three live neighbours dies, as if by overpopulation_
+
+4. _Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction_
+
+## Future Work
+
+There are many features which can be added to this project in particular including:
+
+- CSS Styling on the HTML page where the visualisation is generated
+
+- Recognition of common patterns
+
+- User input of the starting cell values
+
+- Colour indication of the rules each cell has recently followed
+
+- Marking the cells as averages over the number of generations to create a different map
+
+
+## Requires
+
+- P5.js
--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ function make2DArray(columns, rows) {
 }
 
 let grid;
-let width = 400;
-let height = 400;
-let resolution = 40 ;
+let WIDTH = 600;
+let HEIGHT = 600;
+let resolution = 2;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(WIDTH, HEIGHT);
   columns = width / resolution;
   rows = height / resolution;
   grid = make2DArray(columns, rows);
@@ -41,4 +41,39 @@ function draw() {
       }
     }
   }
+
+  // Next Grid
+  let nextGrid = make2DArray(columns, rows);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let sum = 0;
+      let neighbors = nearestNeighbourCount(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        nextGrid[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        nextGrid[i][j] = 0;
+      } else {
+        nextGrid[i][j] = state;
+      }
+    }
+  }
+
+  grid = nextGrid
+}
+
+function nearestNeighbourCount(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + columns) % columns;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
 }
