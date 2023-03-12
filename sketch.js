c2cefcaa737945c809e908f213d8a3e9d92ead4a
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-03-12 19:57:40 +0000

    Dark CSS and Logo

--- sketch.js
+++ sketch.js
@@ -48,15 +48,14 @@ function draw() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       let state = grid[i][j];
-      // Count live neighbors!
       let sum = 0;
       let neighbors = nearestNeighbourCount(grid, i, j);
 
-      if (state == 0 && neighbors == 3) {
+      if (state == 0 && neighbors == 3) { //
         nextGrid[i][j] = 1;
       } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
         nextGrid[i][j] = 0;
-      } else {
+              } else {
         nextGrid[i][j] = state;
       }
     }
